3e37bd0026075bcdf9a3bca468b1f7bb77250ec4
tyobo <cchobo32@gmail.com>
2021-07-16 16:32:07 +0900

    HellBulletTool
    ヘルバトルのツール制作途中

---
+++ HBBTool/HellBulletTool/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace HellBulletTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.Speedtxt = new System.Windows.Forms.TextBox();
+            this.Angletxt = new System.Windows.Forms.TextBox();
+            this.Atktxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 418);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Speedtxt
+            // 
+            this.Speedtxt.Location = new System.Drawing.Point(381, 112);
+            this.Speedtxt.Name = "Speedtxt";
+            this.Speedtxt.Size = new System.Drawing.Size(120, 19);
+            this.Speedtxt.TabIndex = 1;
+            // 
+            // Angletxt
+            // 
+            this.Angletxt.Location = new System.Drawing.Point(381, 137);
+            this.Angletxt.Name = "Angletxt";
+            this.Angletxt.Size = new System.Drawing.Size(120, 19);
+            this.Angletxt.TabIndex = 2;
+            // 
+            // Atktxt
+            // 
+            this.Atktxt.Location = new System.Drawing.Point(381, 162);
+            this.Atktxt.Name = "Atktxt";
+            this.Atktxt.Size = new System.Drawing.Size(120, 19);
+            this.Atktxt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "適用";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "スピード";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "角度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "攻撃力";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(379, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "追尾弾",
+            "散弾"});
+            this.comboBox1.Location = new System.Drawing.Point(381, 86);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 442);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Atktxt);
+            this.Controls.Add(this.Angletxt);
+            this.Controls.Add(this.Speedtxt);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer updateTimer;
+        private System.Windows.Forms.TextBox Speedtxt;
+        private System.Windows.Forms.TextBox Angletxt;
+        private System.Windows.Forms.TextBox Atktxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ HBBTool/HellBulletTool/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HellBulletTool
+{
+    public partial class Form1 : Form
+    {
+        public float m_speed = 0;
+        public float m_atk = 0;
+        public float m_angle = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.White, new RectangleF(0, 0, 10, 10));
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_speed = Convert.ToSingle(Speedtxt.Text);
+            m_atk = Convert.ToSingle(Angletxt.Text);
+            m_angle = Convert.ToSingle(Atktxt.Text);
+
+            label4.Text = ("\nスピード:" + m_speed + "\n", "攻撃力:" + m_atk + "\n", "角度:" + m_angle + "\n").ToString();
+            pictureBox1.Invalidate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+
+                    break;
+                case 1:
+
+                    break;
+
+            }
+
+        }
+    }
+}
+
+/*
+ * 質問メモ
+ * 画像の動かし方
+ * 弾の選択はフォーム遷移でいいのか
+ * コンボボックスで図形変更
+ */
